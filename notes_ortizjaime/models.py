0c064747565f98564510975d47aed79229d47196
VelascoRt <velascogmac@gmail.com>
2024-10-21 05:55:09 -0600

    migrations change

--- notes_ortizjaime/models.py
+++ notes_ortizjaime/models.py
@@ -7,10 +7,10 @@ class User(models.Model):
 
 
 class Note(models.Model):
-    user : models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
-    title : models.CharField(max_length = 200)
-    content : models.TextField()
-    creation_date : models.DateTimeField("Creation date",auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    creation_date = models.DateTimeField("Creation date",auto_now_add=True)
 
     def __str__(self):
         return self.title
